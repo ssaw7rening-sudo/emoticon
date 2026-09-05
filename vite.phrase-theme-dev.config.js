68a52dfdc699911ca333ec87cdeaf69161ff6338
ssaw7rening-sudo <ssaw7.rening@gmail.com>
2026-09-06 02:13:55 +0900

    Use phrase-theme v2 in dev

--- vite.phrase-theme-dev.config.js
+++ vite.phrase-theme-dev.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import baseConfig from './vite.prompt-library-dev.config.js'
-import { phraseThemeExpansion } from './vite.phrase-theme-plugin.js'
+import { phraseThemeExpansionV2 } from './vite.phrase-theme-plugin-v2.js'
 
 export default defineConfig({
   ...baseConfig,
-  plugins: [phraseThemeExpansion(), ...(baseConfig.plugins || [])],
+  plugins: [phraseThemeExpansionV2(), ...(baseConfig.plugins || [])],
 })
