e619ba993fd0915f3e31cf3a97283b9e6cfde249
ssaw7rening-sudo <ssaw7.rening@gmail.com>
2026-09-06 08:17:45 +0900

    Wire phrase theme expansion v3 into production build

--- vite.phrase-theme-build.config.js
+++ vite.phrase-theme-build.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import baseConfig from './vite.prompt-library-build.config.js'
 import { phraseThemeExpansionV2 } from './vite.phrase-theme-plugin-v2.js'
+import { phraseThemeExpansionV3 } from './vite.phrase-theme-plugin-v3.js'
 import { photoReferencePromptStructure } from './vite.photo-reference-prompt-plugin.js'
 
 export default defineConfig({
   ...baseConfig,
-  plugins: [photoReferencePromptStructure(), phraseThemeExpansionV2(), ...(baseConfig.plugins || [])],
+  plugins: [photoReferencePromptStructure(), phraseThemeExpansionV2(), phraseThemeExpansionV3(), ...(baseConfig.plugins || [])],
 })
