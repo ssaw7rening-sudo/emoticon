1d3219a358b6edce597aada400c4ddc8b331e9fe
ssaw7rening-sudo <ssaw7.rening@gmail.com>
2026-09-06 08:13:25 +0900

    Wire photo-reference prompt structure into dev build

--- vite.phrase-theme-dev.config.js
+++ vite.phrase-theme-dev.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import baseConfig from './vite.prompt-library-dev.config.js'
 import { phraseThemeExpansionV2 } from './vite.phrase-theme-plugin-v2.js'
+import { photoReferencePromptStructure } from './vite.photo-reference-prompt-plugin.js'
 
 export default defineConfig({
   ...baseConfig,
-  plugins: [phraseThemeExpansionV2(), ...(baseConfig.plugins || [])],
+  plugins: [photoReferencePromptStructure(), phraseThemeExpansionV2(), ...(baseConfig.plugins || [])],
 })
