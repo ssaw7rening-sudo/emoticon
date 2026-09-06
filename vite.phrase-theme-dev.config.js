39f195d6dae85b054a9855a02e86ae92e0a5450e
ssaw7rening-sudo <ssaw7.rening@gmail.com>
2026-09-06 13:17:20 +0900

    개발 설정에 전체 테마 행동 연동 추가

--- vite.phrase-theme-dev.config.js
+++ vite.phrase-theme-dev.config.js
@@ -9,10 +9,11 @@ import { mainAdCleanupPlugin } from './vite.main-ad-cleanup-plugin.js'
 import { bulkSaveLayoutPlugin } from './vite.bulk-save-layout-plugin.js'
 import { modelLetteringPromptPlugin } from './vite.model-lettering-prompt-plugin.js'
 import { textCharacterInvariancePlugin } from './vite.text-character-invariance-plugin.js'
+import { themeActionExpressionPlugin } from './vite.theme-action-expression-plugin.js'
 import { safeWhiteSheetRoutingPlugin } from './vite.safe-white-sheet-routing-plugin.js'
 import { transparentSheetAspectPlugin } from './vite.transparent-sheet-aspect-plugin.js'
 
 export default defineConfig({
   ...baseConfig,
-  plugins: [transparentSheetAspectPlugin(), safeWhiteSheetRoutingPlugin(), modelLetteringPromptPlugin(), textCharacterInvariancePlugin(), bulkSaveLayoutPlugin(), mainAdCleanupPlugin(), adsensePlacementPlugin(), photoReferencePromptStructure(), phraseThemeExpansionV2(), phraseThemeExpansionV3(), goldenComboRebuildV2(), ...(baseConfig.plugins || [])],
+  plugins: [transparentSheetAspectPlugin(), safeWhiteSheetRoutingPlugin(), modelLetteringPromptPlugin(), textCharacterInvariancePlugin(), themeActionExpressionPlugin(), bulkSaveLayoutPlugin(), mainAdCleanupPlugin(), adsensePlacementPlugin(), photoReferencePromptStructure(), phraseThemeExpansionV2(), phraseThemeExpansionV3(), goldenComboRebuildV2(), ...(baseConfig.plugins || [])],
 })
